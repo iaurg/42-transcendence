891d308f3d2fdb0944225dc93e7b47de1fac6bcd
Italo A <italoaurelior@gmail.com>
2023-06-04 23:09:45 +0000

    feat: sidebar component

---
+++ frontend/src/components/Sidebar/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { Play, SignOut, Trophy } from "@phosphor-icons/react";
+import Image from "next/image";
+
+export default function Sidebar() {
+  return (
+    <div className="flex flex-col h-screen max-wd-[93] bg-black42-300">
+      <div className="flex flex-col flex-1 justify-between items-center">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/images/logo-icon-pong.png"
+            alt="42 logo"
+            width={60}
+            height={60}
+            className="mt-8 mb-8"
+          />
+          <ul className="flex flex-col space-y-6 text-center items-center justify-center">
+            <li>
+              <a
+                href="/game/play"
+                className="text-white font-bold  rounded-full transition-all hover:bg-purple42-200
+                flex items-center justify-center w-12 h-12"
+              >
+                <Play color="white" size={32} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="/game"
+                className="text-white font-bold  rounded-full transition-all hover:bg-purple42-200
+                flex items-center justify-center w-12 h-12"
+              >
+                <Trophy color="white" size={32} />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <ul className="flex flex-col space-y-2 items-center justify-center">
+          <li>
+            <a
+              href="/"
+              className="text-white font-bold py-2 px-4 rounded-lg transition-all hover:bg-purple42-200"
+            >
+              Perfil
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="text-white font-bold  rounded-full transition-all hover:bg-purple42-200
+                flex items-center justify-center w-12 h-12"
+            >
+              <SignOut color="white" size={32} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
