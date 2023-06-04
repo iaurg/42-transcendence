8b9e6c3189804d17901bef0e6ca495111f360c3d
Italo A <italoaurelior@gmail.com>
2023-06-04 23:09:33 +0000

    feat: pattern pages game

---
+++ frontend/src/app/(private)/game/page.tsx
@@ -0,0 +1,3 @@
+export default function GamePage() {
+  return <h1 className="text-white text-2xl">Game Lobby</h1>;
+}
---
+++ frontend/src/app/(private)/game/play/page.tsx
@@ -0,0 +1,3 @@
+export default function PlayPage() {
+  return <h1 className="text-white text-2xl">Play a Game</h1>;
+}
