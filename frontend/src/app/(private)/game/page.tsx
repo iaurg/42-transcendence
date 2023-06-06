0b87da6b42bab4779cb1a38dbea37cb5452a81ce
Italo A <italoaurelior@gmail.com>
2023-06-06 01:02:58 +0000

    feat: start leaderboard

--- frontend/src/app/(private)/game/page.tsx
+++ frontend/src/app/(private)/game/page.tsx
@@ -1,3 +1,9 @@
+import { LeaderBoard } from "@/components/LeaderBoard";
+
 export default function GamePage() {
-  return <h1 className="text-white text-2xl">Game Lobby</h1>;
+  return (
+    <h1 className="text-white text-2xl pt-4">
+      <LeaderBoard />
+    </h1>
+  );
 }
---
+++ frontend/src/components/LeaderBoard/index.tsx
@@ -0,0 +1,7 @@
+export function LeaderBoard() {
+  return (
+    <div className="flex justify-between items-center py-12 bg-black42-300 p-4 rounded-lg">
+      Leaderboard
+    </div>
+  );
+}
