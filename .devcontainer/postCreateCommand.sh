65bc8c08e696858ed14cb133853681e3f47c1583
Juliano Choi <julianochoi@gmail.com>
2023-06-06 20:20:29 -0300

    fix: Adds prisma migrate and docker daemon setup to devcontainer (#32)
    Co-authored-by: Juliano <jchoi@42sp.org.br>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Install dependencies
 cd backend/ && npm ci
 
+# Install NestJS CLI
 npm install -g @nestjs/cli
 
+# Generate Prisma Client
 npm run prisma:generate
+
+# Generate Prisma Migrations
+npx prisma migrate dev --name init
+
+# Docker daemon setup
+chown node:node /var/run/docker.sock
