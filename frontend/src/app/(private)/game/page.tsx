8b518a14ec7e50150c458a5dd4c558ade24271f9
Italo A <italoaurelior@gmail.com>
2023-06-07 23:00:45 +0000

    feat: create leaderboard components

--- frontend/src/app/(private)/game/page.tsx
+++ frontend/src/app/(private)/game/page.tsx
@@ -2,8 +2,8 @@ import { LeaderBoard } from "@/components/LeaderBoard";
 
 export default function GamePage() {
   return (
-    <h1 className="text-white text-2xl pt-4">
+    <div className="mt-4">
       <LeaderBoard />
-    </h1>
+    </div>
   );
 }
--- frontend/src/components/LeaderBoard/index.tsx
+++ frontend/src/components/LeaderBoard/index.tsx
@@ -1,7 +1,45 @@
+import { LeaderBoardCard } from "../LeaderBoardCard";
+
+// array of users objects with name, avatar, score, and isFriend
+const fakeUsers = [
+  {
+    name: "Jorge",
+    avatar: "https://i.pravatar.cc/60",
+    score: 42,
+    isFriend: true,
+  },
+  {
+    name: "Loreona",
+    avatar: "https://i.pravatar.cc/60",
+    score: 1,
+    isFriend: false,
+  },
+  {
+    name: "Gabriela",
+    avatar: "https://i.pravatar.cc/60",
+    score: 0,
+    isFriend: false,
+  },
+  {
+    name: "Mario",
+    avatar: "https://i.pravatar.cc/60",
+    score: 3,
+    isFriend: true,
+  },
+];
+
 export function LeaderBoard() {
   return (
-    <div className="flex justify-between items-center py-12 bg-black42-300 p-4 rounded-lg">
-      Leaderboard
+    <div className="flex flex-col justify-between items-center py-4 bg-black42-300 p-4 rounded-lg space-y-2">
+      {fakeUsers.map((user) => (
+        <LeaderBoardCard
+          key={user.name}
+          name={user.name}
+          avatar={user.avatar}
+          score={user.score}
+          isFriend={user.isFriend}
+        />
+      ))}
     </div>
   );
 }
