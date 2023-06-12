af6da5fdb9e3d874a6b7a480b3dc76b577a162e1
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2023-06-12 20:59:47 -0300

    add: user controllers (#40)
    Co-authored-by: lenzo <lenzo@mail.com>
    Co-authored-by: Juliano <jchoi@42sp.org.br>

---
+++ backend/src/users/dto/createUser.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsString } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  login: string;
+
+  @IsString()
+  displayname: string;
+
+  @IsEmail()
+  email: string;
+}
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,35 +1,47 @@
-import { Controller, Get, Body, Query, Post, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Body,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/user.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { CreateUserDto } from './dto/createUser.dto';
 
-@Controller('user*')
+@Controller('users')
 export class UsersController {
-  constructor(private service: UsersService) { }
-
-  @Post()
-  async create(@Query() userDto: CreateUserDto) {
-    try {
-      const createdUser = await this.service.create(userDto);
-      return { message: "User successfully created", data: createdUser }
-    } catch (error) {
-      throw new HttpException({ error }, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  @Get()
-  async find(@Query() userDto: CreateUserDto) {
-    try {
-      if (userDto.login) {
-        return await this.service.find(userDto);
-      }
-      return await this.service.listAll();
-    } catch (error) {
-      throw new HttpException({ error }, HttpStatus.BAD_REQUEST);
-    }
-  }
+  constructor(private service: UsersService) {}
 
   @Get('me')
   findMe(@Body() dto: any) {
     return this.service.findMe(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':login')
+  findOne(@Param('login') login: string) {
+    return this.service.findOne(login);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.service.create(createUserDto);
+  }
+
+  @Patch(':login')
+  update(@Param('login') login: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.service.update(login, updateUserDto);
+  }
+
+  @Delete(':login')
+  remove(@Param('login') login: string) {
+    return this.service.remove(login);
   }
 }
