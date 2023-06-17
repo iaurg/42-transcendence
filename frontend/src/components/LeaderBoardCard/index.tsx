053c7e19d2b6d6eb278ca7279442a071caed626e
Italo A <italoaurelior@gmail.com>
2023-06-17 23:02:47 +0000

    feat: create profile public popover

--- frontend/src/components/LeaderBoardCard/index.tsx
+++ frontend/src/components/LeaderBoardCard/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Trophy, UserPlus } from "@phosphor-icons/react";
 import { StatusTag } from "../StatusTag";
+import ProfilePopOver from "../ProfilePopOver";
 
 type LeaderBoardCardProps = {
   name: string;
@@ -17,16 +18,18 @@ export function LeaderBoardCard({
 }: LeaderBoardCardProps) {
   return (
     <div className="flex flex-row justify-between items-center py-3 bg-black42-200 p-4 rounded-lg w-full">
-      <div className="flex flex-row justify-between items-center cursor-pointer">
-        <img
-          src={`${avatar}?u=${name}`}
-          width={40}
-          height={40}
-          className="rounded-full mr-3"
-        />
-        <div className="text-white text-lg mr-3">{name}</div>
-        <StatusTag status="offline" />
-      </div>
+      <ProfilePopOver name={name} score={score}>
+        <div className="flex flex-row justify-between items-center">
+          <img
+            src={`${avatar}?u=${name}`}
+            width={40}
+            height={40}
+            className="rounded-full mr-3"
+          />
+          <div className="text-white text-lg mr-3">{name}</div>
+          <StatusTag status="offline" />
+        </div>
+      </ProfilePopOver>
       <div className="flex flex-row justify-between items-center space-x-4">
         <div className="flex flex-row justify-start items-center w-[50px]">
           <Trophy className="text-purple42-200 mr-2" size={24} />
