d474988a6526c0e986667104ddd2615e460c30ce
caio-vinicius <contato@caiovinicius.dev.br>
2023-06-18 14:43:48 -0300

    creation of the raw virtual machine in the infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "transcendence"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 2
+  memory           = 2048
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 32
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+}
+
+output "ip" {
+  value = vsphere_virtual_machine.vm.network_interface.0.ipv4_address
+}
---
+++ terraform/template.variables.tf
@@ -0,0 +1,41 @@
+variable "vsphere_user" {
+  description = "vSphere user"
+  type = string
+  default     = ""
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+  type = string
+  default     = ""
+}
+
+variable "vsphere_server" {
+  description = "vSphere server"
+  type = string
+  default     = ""
+}
+
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+  type = string
+  default     = ""
+}
+
+variable "vsphere_datastore" {
+  description = "vSphere datastore"
+  type = string
+  default     = ""
+}
+
+variable "vsphere_cluster" {
+  description = "vSphere cluster"
+  type = string
+  default     = ""
+}
+
+variable "vsphere_network" {
+  description = "vSphere network"
+  type = string
+  default     = ""
+}
