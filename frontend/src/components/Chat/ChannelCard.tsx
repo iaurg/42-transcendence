3252800a52deffc0c1b0a9447607a94918dd17a4
Italo A <italoaurelior@gmail.com>
2023-06-18 01:12:23 +0000

    feat: create chat bubbles inside opened channel

--- frontend/src/components/Chat/ChannelCard.tsx
+++ frontend/src/components/Chat/ChannelCard.tsx
@@ -1,11 +1,14 @@
 import { ArrowRight, Crown, Lock, TrashSimple } from "@phosphor-icons/react";
-import { use, useEffect } from "react";
+import { use, useContext, useEffect } from "react";
+import Chat from ".";
+import { ChatContext } from "@/contexts/ChatContext";
 
 type ChannelCardProps = {
   name: string;
 };
 
 export default function ChannelCard({ name }: ChannelCardProps) {
+  const { setShowElement, setSelectedChannelId } = useContext(ChatContext);
   const isPrivate = true;
   const isOwner = true;
 
@@ -13,10 +16,17 @@ export default function ChannelCard({ name }: ChannelCardProps) {
     console.log("deletando canal");
   };
 
+  const handleOpenChannel = () => {
+    setSelectedChannelId(name);
+    setShowElement("showChannelOpen");
+  };
+
   return (
     <div className="bg-black42-200 flex justify-between rounded-lg items-center p-3 my-1">
       <div className="flex space-x-2 items-center">
-        <span className="cursor-pointer">{name}</span>
+        <span className="cursor-pointer" onClick={() => handleOpenChannel()}>
+          {name}
+        </span>
         <div className="flex ml-1 space-x-1">
           {isPrivate && <Lock color="white" size={12} />}
           {isOwner && <Crown className="text-orange42-500" size={12} />}
@@ -30,7 +40,11 @@ export default function ChannelCard({ name }: ChannelCardProps) {
             onClick={handleDeleteChannel}
           />
         )}
-        <ArrowRight className="text-purple42-200 cursor-pointer" size={18} />
+        <ArrowRight
+          className="text-purple42-200 cursor-pointer"
+          size={18}
+          onClick={() => handleOpenChannel()}
+        />
       </div>
     </div>
   );
