188825b61b157365900a1bb1b8af3f724e437b79
Italo A <italoaurelior@gmail.com>
2023-10-05 01:45:16 +0000

    refactor: remove old lobby

--- backend/src/lobby/lobby.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LobbyService } from './lobby.service';
-import { LobbyController } from './lobby.controller';
-
-@Module({
-  providers: [LobbyService],
-  controllers: [LobbyController],
-})
-export class LobbyModule {}
