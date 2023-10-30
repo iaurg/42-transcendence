55522f17c5860bc72ecf51e80d184c9a97501bcf
Italo A <italoaurelior@gmail.com>
2023-10-30 00:07:49 +0000

    feat: create a component for user avatar

--- frontend/src/components/UserInfo/index.tsx
+++ frontend/src/components/UserInfo/index.tsx
@@ -4,6 +4,7 @@ import MFAModal from "../MFAModal";
 import { useContext } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
 import EditUserModal from "../EditUserModal";
+import UserAvatar from "../UserAvatar";
 
 export function UserInfo() {
   const { user } = useContext(AuthContext);
@@ -11,18 +12,7 @@ export function UserInfo() {
     <div className="flex justify-between items-center">
       <div className="flex items-center space-x-3">
         <div>
-          {user.avatar ? (
-            <img
-              src={`${user.avatar}`}
-              width={40}
-              height={40}
-              className="rounded-full mr-3"
-            />
-          ) : (
-            <div className="rounded-full mr-3 bg-purple42-200 w-10 h-10 flex justify-center items-center">
-              <span className="text-white text-2xl">todo</span>
-            </div>
-          )}
+          <UserAvatar imageUrl={user.avatar} login={user.displayName} />
         </div>
         <div className="text-white">
           <div className="text-lg">{user.displayName}</div>{" "}
