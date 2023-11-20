4a59f605bc25e58e680dc050842677a1405014c3
Italo A <italoaurelior@gmail.com>
2023-11-20 13:32:09 -0300

    fix: missing origin key

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    [
+    origin:[
       process.env.FRONTEND_URL,
       'http://localhost:3000',
       'http://42transcendence.me',
