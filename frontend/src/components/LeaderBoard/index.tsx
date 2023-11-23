43321df2996410f34eaa7ad24d75a73e25d671bd
Italo A <italoaurelior@gmail.com>
2023-11-22 22:36:11 -0300

    style: better height disposal

--- frontend/src/components/LeaderBoard/index.tsx
+++ frontend/src/components/LeaderBoard/index.tsx
@@ -8,11 +8,11 @@ export function LeaderBoard() {
 
   return (
     <div
-      className="flex flex-col justify-between items-center py-4 bg-black42-300 p-4 rounded-lg space-y-2 max-h-[80vh] overflow-y-scroll overscroll-contain
-    scrollbar scrollbar-w-1 scrollbar-rounded-lg scrollbar-thumb-rounded-lg scrollbar-thumb-black42-100 scrollbar-track-black42-300"
+      className="flex flex-col justify-between items-center py-4 bg-black42-300 p-4 rounded-lg space-y-2 overflow-y-scroll overscroll-contain
+    scrollbar scrollbar-w-1 scrollbar-rounded-lg scrollbar-thumb-rounded-lg scrollbar-thumb-black42-100 scrollbar-track-black42-300 h-[calc(100vh-130px)]"
     >
       {isLoading ? (
-        <div className="flex flex-col flex-1 justify-center items-center">
+        <div className="flex flex-col flex-1 justify-center items-center ">
           <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-purple42-200"></div>
           <span className="text-white text-lg mt-4">Carregando...</span>
         </div>
