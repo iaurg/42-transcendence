0aa482a55024e1469ac720d297cb88edba691a83
Italo A <italoaurelior@gmail.com>
2023-11-28 20:36:01 -0300

    Feat/front block user (#144)
    * feat: add block button
    
    * feat: replicate button pattern
    
    * fix: avatar wrong initial letter

--- frontend/src/components/Chat/FriendCard.tsx
+++ frontend/src/components/Chat/FriendCard.tsx
@@ -66,15 +66,21 @@ export default function FriendCard({
           } w-2 h-2 rounded-full`}
         ></div>
       </div>
-      <div className="flex space-x-5 items-center">
+      <div className="flex space-x-2 items-center">
         <EnvelopeSimple
-          className="text-purple42-200 cursor-pointer"
+          className="text-white font-bold rounded-lg bg-purple42-200 transition-all hover:bg-purple42-300
+          flex items-center justify-center w-8 h-8 p-2 cursor-pointer"
           size={18}
           onClick={handleOpenDirectMessage}
         />
-        <Sword className="text-purple42-200" size={18} />
+        <Sword
+          className="text-white font-bold rounded-lg bg-purple42-200 transition-all hover:bg-purple42-300
+                      flex items-center justify-center w-8 h-8 p-2 cursor-pointer"
+          size={18}
+        />
         <UserMinus
-          className="text-purple42-200 cursor-pointer"
+          className="text-white font-bold rounded-lg bg-purple42-200 transition-all hover:bg-purple42-300
+          flex items-center justify-center w-8 h-8 p-2 cursor-pointer"
           size={18}
           onClick={handleDeleteFriend}
         />
