7464b3d4214171a3dbfb42027c06b669904dd373
Italo A <italoaurelior@gmail.com>
2023-12-03 23:03:17 -0300

    fix: filter return of user and validate file sent

--- backend/src/avatar-upload/avatar-upload.service.ts
+++ backend/src/avatar-upload/avatar-upload.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -7,9 +11,16 @@ export class AvatarUploadService {
 
   async create(id: string, file: Express.Multer.File) {
     const { filename } = file;
+
+    // Validate the file type and size
+    if (!file.mimetype.startsWith('image/') || file.size > 1024 * 1024) {
+      throw new BadRequestException('Invalid file');
+    }
+
     const user = await this.prisma.user.update({
       where: { id: id },
       data: { avatar: filename },
+      select: { id: true, avatar: true }, // Select only the fields that are safe to return
     });
 
     return user;
@@ -17,9 +28,16 @@ export class AvatarUploadService {
 
   async update(id: string, file: Express.Multer.File) {
     const { filename } = file;
+
+    // Validate the file type and size
+    if (!file.mimetype.startsWith('image/') || file.size > 1024 * 1024) {
+      throw new BadRequestException('Invalid file');
+    }
+
     const user = await this.prisma.user.update({
       where: { id: id },
       data: { avatar: filename },
+      select: { id: true, avatar: true }, // Select only the fields that are safe to return
     });
 
     return user;
